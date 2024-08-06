f8c4782297a5918a8b18d9ab9828022af8cb6300
Antonina Sych <antoninasych@gmail.com>
2024-08-06 18:37:15 +0100

    changed structure

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,39 +1,12 @@
-$.getJSON(URL, function(data) {
-  $('.loader').addClass('hidden');
-  if (parseInt(data.totalHits) > 0) {
-    $.each(data.hits, function(i, hit) {
-      $('.gallery').append(
-        '<div class="photo-card">' +
-        '<a href="' + hit.largeImageURL + '" class="gallery-link">' +
-        '<img src="' + hit.previewURL + '" alt="' + hit.tags + '" loading="lazy" />' +
-        '</a>' +
-        '<div class="info">' +
-        '<p class="info-item"><b>Likes</b> ' + hit.likes + '</p>' +
-        '<p class="info-item"><b>Views</b> ' + hit.views + '</p>' +
-        '<p class="info-item"><b>Comments</b> ' + hit.comments + '</p>' +
-        '<p class="info-item"><b>Downloads</b> ' + hit.downloads + '</p>' +
-        '</div>' +
-        '</div>'
-      );
-    });
-    var lightbox = new SimpleLightbox('.gallery a');
-    lightbox.refresh();
-  } else {
-    iziToast.error({
-      title: 'No results',
-      message: 'Sorry, there are no images matching your search query. Please try again!',
-      position: 'topRight',
-      timeout: 5000
-    });
+const API_KEY = '45260330-4aac6e6a6c993f9154e4b36c5';
+
+export async function fetchImages(query, page = 1, perPage = 12) {
+  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`;
+  try {
+    const response = await fetch(URL);
+    if (!response.ok) throw new Error('Network response was not ok');
+    return await response.json();
+  } catch (error) {
+    throw error;
   }
-}).fail(function(jqxhr, textStatus, error) {
-  $('.loader').addClass('hidden');
-  var err = textStatus + ", " + error;
-  console.log("Request Failed: " + err);
-  iziToast.error({
-    title: 'Error',
-    message: 'An error occurred while fetching images.',
-    position: 'topRight',
-    timeout: 5000
-  });
-});
+}
